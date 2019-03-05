d3ebc924e1d8394db18bf41dff087248e9c1c5cb
Spin <spinshanxi@gmail.com>
2019-03-05 12:50:30 +0800

    modify dockerfile

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -118,5 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+print('STATIC_ROOT', STATIC_ROOT)
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+INTERNAL_IPS = ['127.0.0.1']
