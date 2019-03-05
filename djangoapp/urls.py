777894b07d8a1480fef15988489ee99dd7ce5daf
Spin <spinshanxi@gmail.com>
2019-03-06 00:11:00 +0800

    modify url

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -23,6 +23,7 @@ admin.autodiscover()
 router = DefaultRouter()
 router.register(r"todo", TaskViewSet)
 urlpatterns = [
+	path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
