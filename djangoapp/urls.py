0162f36fdd19e8a3efa03299006ebbd5d351fe92
Spin <spinshanxi@gmail.com>
2019-03-05 13:58:51 +0800

    add rest framework

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import include, url
+from rest_framework.routers import DefaultRouter
+from todo_app.todo.views import TaskViewSet
+admin.autodiscover()
+router = DefaultRouter()
+router.register(r"todo", TaskViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
